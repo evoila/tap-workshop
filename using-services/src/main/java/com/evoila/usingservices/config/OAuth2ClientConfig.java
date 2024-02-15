2e99661b80962c9f8c774da108f324f63f325049
MaxF <maxfuhrich@gmail.com>
2024-02-15 19:30:02 +0100

    binding for appsso

--- using-services/src/main/java/com/evoila/usingservices/config/OAuth2ClientConfig.java
+++ using-services/src/main/java/com/evoila/usingservices/config/OAuth2ClientConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.ClientRegistrations;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 
 @Configuration
 @EnableWebSecurity
@@ -27,9 +29,13 @@ public class OAuth2ClientConfig {
             System.exit(1);
         }
         return ClientRegistrations.fromIssuerLocation(bindings[0].get("issuer-uri"))
-                .registrationId(bindings[0].get("provider"))
+                //.registrationId(bindings[0].get("provider"))
+                .registrationId("appsso")
                 .clientId(bindings[0].get("client-id"))
                 .clientSecret(bindings[0].get("client-secret"))
+                .authorizationGrantType(new AuthorizationGrantType(bindings[0].get("authorization-grant-type")))
+                .clientAuthenticationMethod(new ClientAuthenticationMethod(bindings[0].get("client-authentication-method")))
+                .scope(bindings[0].get("scope"))
                 .redirectUri("https://mf-mongodb-client-sso.dev-ns-03.tap.dieunkrauts.de/login/oauth2/code/appsso")
                 .build();
     }
