636fb865a5d68f43d49926d060df78240ae20ad1
MaxF <maxfuhrich@gmail.com>
2024-02-15 18:48:40 +0100

    tr

--- using-services/src/main/java/com/evoila/usingservices/config/OAuth2ClientConfig.java
+++ using-services/src/main/java/com/evoila/usingservices/config/OAuth2ClientConfig.java
@@ -1,9 +1,13 @@
 package com.evoila.usingservices.config;
 
+import com.nebhale.bindings.Binding;
+import com.nebhale.bindings.Bindings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ClientRegistrations;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 
 @Configuration
@@ -12,6 +16,21 @@ public class OAuth2ClientConfig {
 
     @Bean
     public ClientRegistrationRepository clientRegistrationRepository() {
-        return new InMemoryClientRegistrationRepository();
+        return new InMemoryClientRegistrationRepository(getAppSsoClientRegistration());
+    }
+
+    private ClientRegistration getAppSsoClientRegistration() {
+        Binding[] bindings = Bindings.fromServiceBindingRoot();
+        bindings = Bindings.filter(bindings, "appsso");
+        if (bindings.length != 1) {
+            System.err.printf("Incorrect number of appsso drivers: %d\n", bindings.length);
+            System.exit(1);
+        }
+        return ClientRegistrations.fromIssuerLocation(bindings[0].get("issuer-uri"))
+                .registrationId(bindings[0].get("provider"))
+                .clientId(bindings[0].get("client-id"))
+                .clientSecret(bindings[0].get("client-secret"))
+                .redirectUri("https://mf-mongodb-client-sso.dev-ns-03.tap.dieunkrauts.de/login/oauth2/code/appsso")
+                .build();
     }
 }
