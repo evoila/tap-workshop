84508518316404b14c5e8b3968a8d0fd1d90afbb
MaxF <maxfuhrich@gmail.com>
2024-02-20 14:39:08 +0100

    enable api auto registration add cors-response headers through CorsConfig

---
+++ using-services/src/main/java/com/evoila/usingservices/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.evoila.usingservices.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+/*
+ * Since the developer portal requires CORS-headers for handling a response when using the api-entity created by the auto registration,
+ * this configuration was added.
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
